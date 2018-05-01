b77e96c66885cb15981b9cc4e9da4f791c44959d
Timothy Winfred <twinfred@gmail.com>
2018-05-01 13:02:58 -0700

    first commit

---
+++ LandingPage/app.js
@@ -0,0 +1,41 @@
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer(function (request, response){
+    console.log('client request URL: ', request.url);
+    if(request.url === '/') {
+        fs.readFile('index.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents);
+            response.end();
+        });
+    }
+    else if(request.url === '/ninjas') {
+        fs.readFile('ninjas.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents);
+            response.end();
+        });
+    }
+    else if(request.url === '/dojos/new') {
+        fs.readFile('dojos.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents);
+            response.end();
+        });
+    }
+    else if(request.url === '/stylesheets/style.css'){
+        fs.readFile('stylesheets/style.css', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/css'});
+            response.write(contents);
+            response.end();
+        });
+    }
+    else {
+        response.writeHead(404);
+        response.end('File not found!!!');
+    }
+});
+// tell your server which port to run on
+server.listen(6789);
+// print to terminal window
+console.log("Running in localhost at port 6789");
---
+++ test.js
@@ -0,0 +1,2 @@
+console.log('test running from node')
+console.log('edited')
